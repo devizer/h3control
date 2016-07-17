04aa5eccffe79b5260aaab37f5dc8a9125cc2558
devizer <vgoyda@gmail.com>
2016-07-17 05:48:06 +0300

    update

---
+++ H3Control.Tests/Integration_Tests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H3Control.Tests
+{
+    using System.Diagnostics;
+
+    using Common;
+
+    using NUnit.Framework;
+
+    using Universe;
+
+    [TestFixture]
+    public class Integration_Tests
+    {
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            CrossInfo.AttachUnitTrace("H3Control unit tests");
+        }
+        
+        [Test]
+        public void T01_Platform()
+        {
+            Trace.WriteLine("Platform:" + CrossInfo.ThePlatform);
+        }
+
+        [Test]
+        public void T02_CPU()
+        {
+            Trace.WriteLine("CPU: " + CrossInfo.ProcessorName);
+        }
+
+        [Test]
+        public void T03_Memory()
+        {
+            Trace.WriteLine("Memory: " + CrossInfo.TotalMemory);
+        }
+
+        [Test]
+        public void T03_Processes()
+        {
+            List<PsProcessInfo> list = PsListener_OnLinux.Select(PsSortOrder.Cpu, 99999);
+            Trace.WriteLine("Total processes: " + list.Count);
+            foreach (var p in list)
+            {
+                Trace.WriteLine("     " + p);
+            }
+
+            Trace.WriteLine("");
+        }
+
+    }
+}
