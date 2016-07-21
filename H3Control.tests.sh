619a5edc549aa3246077610d371b8d7a9c38ba62
devizer <vgoyda@gmail.com>
2016-07-21 05:06:59 +0300

    NUnit ConsoleRunner 3.4.1

--- H3Control.tests.sh
+++ H3Control.tests.sh
@@ -1,1 +1,3 @@
 mono --debug --desktop packages/NUnit.ConsoleRunner.3.4.1/tools/nunit3-console.exe --labels=On --workers=1 --work=H3Control.Tests/bin/Debug H3Control.Tests/bin/Debug/H3Control.Tests.dll
+echo
+echo TESTS RAN ON: `mono --version | head -1`
