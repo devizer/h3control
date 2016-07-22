aec3b1a179fb8ac11dd044c2b82d0b20fe2a221c
devizer <vgoyda@gmail.com>
2016-07-23 02:58:39 +0300

    update

--- H3Control/Properties/AssemblyVersion.cs
+++ H3Control/Properties/AssemblyVersion.cs
@@ -1,4 +1,4 @@
 
-   [assembly: System.Reflection.AssemblyVersion("1.13.489.0")]
-   [assembly: Universe.AssemblyBuildDateTime("Fri, 22 Jul 2016 22:34:12 GMT")]
+   [assembly: System.Reflection.AssemblyVersion("1.13.490.0")]
+   [assembly: Universe.AssemblyBuildDateTime("Fri, 22 Jul 2016 23:51:45 GMT")]
 
