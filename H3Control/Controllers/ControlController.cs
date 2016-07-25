4e23faf0481d7904ab36b648288e46e5ee6e9c9e
devizer <vgoyda@gmail.com>
2016-07-25 11:52:47 +0300

    Improved /api/control/ddr/{freq}

--- H3Control/Controllers/ControlController.cs
+++ H3Control/Controllers/ControlController.cs
@@ -61,8 +61,8 @@ namespace H3Control.Controllers
                         File.WriteAllText(pathMin, freq + "000");
                         File.WriteAllText(pathMax, freq + "000");
                         File.WriteAllText(pathCur, freq + "000");
-                        Thread.Sleep(111);
-                        File.WriteAllText(pathMin, 408 + "000");
+                        Thread.Sleep(1); // doesnt work
+                        File.WriteAllText(pathMin, "408" + "000");
 
                         return new ControlStatus() {IsOk = true};
                     }
