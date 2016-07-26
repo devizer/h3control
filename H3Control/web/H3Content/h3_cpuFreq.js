d60eed03cdaf1247f94390a4e2e3ff1f56b14efb
devizer <vgoyda@gmail.com>
2016-07-26 18:27:02 +0300

    fixed min of CPU Gauge

--- H3Control/web/H3Content/h3_cpuFreq.js
+++ H3Control/web/H3Content/h3_cpuFreq.js
@@ -11,17 +11,17 @@ function cpuFreq_OnReady() {
 
     gauge_CpuFreq.jqxGauge({
         ranges: [
-            { startValue: 100, endValue: 700, style: { fill: '#4bb648', stroke: '#4bb648' }, endWidth: 4, startWidth: 2 },
+            { startValue: 50, endValue: 700, style: { fill: '#4bb648', stroke: '#4bb648' }, endWidth: 4, startWidth: 2 },
             { startValue: 700, endValue: 1000, style: { fill: '#fbd109', stroke: '#fbd109' }, endWidth: 6, startWidth: 4 },
             { startValue: 1000, endValue: 1300, style: { fill: '#ff8000', stroke: '#ff8000' }, endWidth: 8, startWidth: 6 },
-            { startValue: 1300, endValue: 1600, style: { fill: '#e02629', stroke: '#e02629' }, endWidth: 10, startWidth: 8 }
+            { startValue: 1300, endValue: 1550, style: { fill: '#e02629', stroke: '#e02629' }, endWidth: 10, startWidth: 8 }
         ],
         ticksMinor: { interval: 125/2, size: '4%' },
         ticksMajor: { interval: 250, size: '8%' },
         width: 259,
         height: 259,
-        min: 60,
-        max: 1600,
+        min: 50,
+        max: 1550,
         value: 10,
         colorScheme: 'scheme05',
         animationDuration: 700,
