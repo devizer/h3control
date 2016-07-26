878b714c1aacc34b021cc73dec8fe3b623f2473e
devizer <vgoyda@gmail.com>
2016-07-26 17:47:41 +0300

    BindCpuUsage reformatted

--- H3Control/web/H3Content/h3_cpuUsage.js
+++ H3Control/web/H3Content/h3_cpuUsage.js
@@ -22,18 +22,18 @@ function BindCpuUsage(usage, el) {
         var item = usage[i];
         var w = parseInt(Math.round(item.Value * onePerCentWidth));
         if (w < 2) w = 2;
-        if (i == usage.length - 1) w = parentWidth - wTotal;
+        var isLast = i === (usage.length - 1);
+        if (isLast) w = parentWidth - wTotal;
         if (w < 1) w = 1;
         wTotal += w;
-        var style2 = "";
-        if (i + 1 < usage.length) style2 = "border-right: 1px solid #DDDDDD";
-        var labelWidth = 60;
-        var sectionClass = (w >= labelWidth) ? "CpuUsageLabel" : "CpuUsageNoLabel";
-        var sectionLabel = (w < labelWidth) ? "&nbsp;" : ((Math.round(item.Value * 10) / 10) + "%");
-        htm += "<div class='" + UsageStyles[i]
-            + " CpuUsageBarHeight " + sectionClass + " " +
-            UsageStyles[i] + "Foreground' style='float: left; width: " + w + "px; "
-            + style2 + "'>" + sectionLabel + "</div>";
+        var minWidth = 60;
+        var sectionClass = (w >= minWidth) ? "CpuUsageSection-WithLabel" : "CpuUsageSection-WithoutLabel";
+        var sectionLabel = (w < minWidth) ? "&nbsp;" : ((Math.round(item.Value * 10) / 10) + "%");
+        var allClasses = "CpuUsageSection " + UsageStyles[i] + " " + sectionClass + " " + UsageStyles[i] + "Foreground " + sectionClass;
+        htm += "<div style='width: " + w + "px;' "
+            + "class='" + allClasses + "'>"
+            + sectionLabel
+            + "</div>";
     }
 
     el.html(htm);
