61105db894fc846e268c1b0d481157c337e592b6
devizer <vgoyda@gmail.com>
2016-08-03 06:08:09 +0300

    update

---
+++ travis-plugins/show-perfomance.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+END=40
+echo ""
+echo "[t][tr][th]Threads[/th][th]Perfomance[/th][/tr]"
+for ((i=1;i<=END;i++)); do
+    perf=$(sysbench --test=cpu --cpu-max-prime=20000 --max-time=1 --num-threads=$i run | grep "total number of events" | awk '{print $NF}')
+    printf "[tr][td] %5s [/td] [td] %10s [/td][/tr]\n" "$i" "$perf"
+done
+echo "[/t]"
+echo ""
+
