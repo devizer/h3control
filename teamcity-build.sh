c60964fc644d1d26f2fa340e8ed7886fe826f56d
devizer <vgoyda@gmail.com>
2016-09-27 05:27:20 +0300

    fixed teamcity-build.sh

--- teamcity-build.sh
+++ teamcity-build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+set -e
 echo MY DIR is `pwd`
 printenv
 nuget restore H3Control.sln 
 xbuild H3Control.sln /t:Rebuild /p:Configuration=Debug /verbosity:minimal
-bash H3Control.tests.sh
+mono --debug --desktop packages/NUnit.ConsoleRunner.3.4.1/tools/nunit3-console.exe --labels=On --workers=1 --work=H3Control.Tests/bin/Debug H3Control.Tests/bin/Debug/H3Control.Tests.dll
+# bash H3Control.tests.sh
