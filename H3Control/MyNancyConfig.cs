daa5a41586702723b9952b5ac2306210edc035a1
Volodymyr Hoida <devizer@users.github.com>
2017-12-02 05:22:09 +0300

    Fixed build

--- H3Control/MyNancyConfig.cs
+++ H3Control/MyNancyConfig.cs
@@ -6,7 +6,6 @@
     using System.IO;
     using System.Linq;
     using System.Reflection;
-    using System.Security.Claims;
     using System.Security.Principal;
     using System.Text.RegularExpressions;
 
