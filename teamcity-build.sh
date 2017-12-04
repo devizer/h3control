1ff66d7464144c1b67cdc1d6a8de56120ec578b9
Volodymyr Hoida <devizer@users.github.com>
2017-12-04 03:38:37 +0300

    update

--- teamcity-build.sh
+++ teamcity-build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 echo MY DIR is `pwd`
-nuget3 | head -n 1
+nuget2 | head -n 1
 # printenv
-rm -rf packages; nuget3 restore H3Control.sln
+rm -rf packages; nuget2 restore H3Control.sln
 xbuild H3Control.sln /t:Rebuild /p:Configuration=Debug /verbosity:minimal
 mono --debug --desktop packages/NUnit.ConsoleRunner.3.4.1/tools/nunit3-console.exe --labels=On --workers=1 --work=H3Control.Tests/bin/Debug H3Control.Tests/bin/Debug/H3Control.Tests.dll
 # bash H3Control.tests.sh
