bd28a037455caac193712cccf94cf355ce8ca31a
Volodymyr Hoida <devizer@users.github.com>
2017-12-04 06:55:30 +0300

    update

--- H3Control.tests.sh
+++ H3Control.tests.sh
@@ -1,3 +1,3 @@
-mono --debug --desktop packages/NUnit.ConsoleRunner.3.4.1/tools/nunit3-console.exe --labels=On --workers=1 --work=H3Control.Tests/bin/Debug H3Control.Tests/bin/Debug/H3Control.Tests.dll
-echo
-echo TESTS RAN ON: `mono --version | head -1`
+pushd packages/NUnit.ConsoleRunner.*/tools; export RUNNER_PATH=$(pwd); popd; echo RUNNER_PATH: $RUNNER_PATH;
+mono --debug --desktop $RUNNER_PATH/nunit3-console.exe --labels=On --workers=1 --work=H3Control.Tests/bin/Debug H3Control.Tests/bin/Debug/H3Control.Tests.dll
+echo -e "\n"; echo TESTS RAN ON: `mono --version | head -1`
