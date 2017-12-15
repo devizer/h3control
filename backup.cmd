ee44c932be57b51ae3da9146e853647944cfffb1
Volodymyr Hoida <devizer@users.github.com>
2017-12-15 07:44:16 +0300

    Updated http-server and ASP.NET MVC

--- backup.cmd
+++ backup.cmd
@@ -10,5 +10,5 @@ rd /q /s H3Control.Tests\bin
 rd /q /s H3Control.Tests\obj
 set /p Build=<H3Control\linux-build\_build-number.txt
 set /p Version=<H3Control\linux-build\_version-number.txt 
-"C:\Program Files\7-Zip\7zG.exe" a -t7z -mx=9 -mfb=128 -md=128m -ms=on -xr!.git ^
+"C:\Program Files\7-Zip\7zG.exe" a -t7z -mx=9 -mfb=128 -md=128m -ms=on -xr!.git -xr!.vs ^
   "C:\Users\Backups on Google Drive\H3Control (%datetime%) %Version%.%Build%.7z" .
