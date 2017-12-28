3eb3ee425cb21f8931109cb1f78959734c41f975
Vladimir Goyda <vgoyda@gmail.com>
2017-12-28 20:59:59 +0300

    updated

--- tmp-clone-and-test.sh
+++ tmp-clone-and-test.sh
@@ -1,8 +1,4 @@
-cd /tmp
-work=/tmp/h3-temp
-rm -rf $work
-mkdir -p $work
-cd $work
-git clone https://github.com/devizer/H3Control
-cd H3Control
-bash teamcity-build.sh
+cd /tmp; work=/tmp/h3-temp; rm -rf $work; mkdir -p $work; cd $work; git clone https://github.com/devizer/H3Control; \
+  cd H3Control; \
+  bash teamcity-build.sh
+
