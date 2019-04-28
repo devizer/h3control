fe240f2e667af4ade74a2d6db91b3f7385526e0b
devizer <devizer@users.github.com>
2019-04-28 09:30:42 +0300

    Added support of modern armbian kernel (step 4)

--- H3Control/Properties/AssemblyVersion.cs
+++ H3Control/Properties/AssemblyVersion.cs
@@ -1,4 +1,4 @@
 
-   [assembly: System.Reflection.AssemblyVersion("1.25.8.0")]
+   [assembly: System.Reflection.AssemblyVersion("1.32.666.0")]
    [assembly: Universe.AssemblyBuildDateTime("Mon, 08 Aug 2016 05:12:41 GMT")]
 
